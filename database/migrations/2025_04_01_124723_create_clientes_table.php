496782aa9f9ec5310c14d2b66a9e4d0ee3cc5a8f
eiianacarolina <anacarolinasouzagomes19@gmail.com>
2025-04-01 10:14:25 -0300

    Adicionando a opção de cadastrar na página de login

---
+++ database/migrations/2025_04_01_124723_create_clientes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->string('nome', 80)->nullable(true);
+            $table->string('endereco')->nullable(true);
+            $table->string('telefone')->nullable(true);
+            $table->string('cpf', 11)->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
